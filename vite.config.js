3c9ceb9482439ca7ff1ea72b71e0b79b4eca73f7
Yash Paghadar <yashkpaghadar1809@gmail.com>
2025-06-18 12:43:00 +0530

    After Deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: './src',
+  root: '.',
   plugins: [react()],
   server: {
     port: 3000,
@@ -16,13 +16,9 @@ export default defineConfig({
   },
   base: './',
   build: {
-    outDir: './dist',
+    outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: './src/main.jsx',
-      },
-    },
+
   },
   preview: {
     port: 4173,
